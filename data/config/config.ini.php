20977e5722f7904c31df7b23e821b35e546c4ecf
ttflowerboys <ttflowerboys@gmail.com>
2017-07-09 14:19:00 +0800

    UPDATA: data bakup and config.

---
+++ data/config/config.ini.php
@@ -0,0 +1,116 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'site_name' => '绘佳教育',
+  'site_theme' => 'default',
+  'site_mobile' => '0',
+  'wap' => 'm',
+  'site_title' => '绘佳教育',
+  'site_keywords' => '绘佳教育',
+  'site_description' => '绘佳教育',
+  'site_status' => '1|正常
+2|头条
+3|推荐
+0|未审核',
+  'site_download_image' => '0',
+  'site_watermark' => '0',
+  'site_watermark_pos' => '5',
+  'member_modelid' => '5',
+  'member_level' => '1|二级U1-10
+2|二级U11-20
+3|二级U21-28
+4|三级U1-10
+5|三级U11-20
+6|三级U21-30',
+  'member_class' => '1|BW2
+2|BW3
+3|BW4
+4|BW5
+5|BW6
+6|BW7
+7|BW8
+8|BW9
+9|BW10
+10|BW11
+11|BW12
+12|BW13
+14|BW14
+15|BW15
+16|BW16
+17|BW17
+18|BW18
+19|BW19
+20|BW20
+21|BW21
+22|BW22
+23|BW23
+24|BW24
+25|BW25
+26|BW26
+27|BW27
+28|BW28
+29|BW29
+30|BW30
+31|SD1
+32|SD2
+33|SD3
+34|SD4
+35|SD5
+36|SD6
+37|SD7
+38|SD8
+39|SD9
+40|SD10
+41|SD11
+42|SD12
+43|SD13
+44|SD14
+45|SD15
+46|SD16
+47|SD17
+48|SD18
+49|SD19
+50|SD20
+51|SD21
+52|SD22
+53|SD23
+54|SD24
+55|SD25
+56|SD26
+57|SD27
+58|SD28
+59|SD29
+60|SD30
+61|SD31
+62|SD32
+63|SD33
+64|SD34
+65|SD35
+66|SD36
+67|SD37
+68|SD38
+69|SD39
+70|SD40
+71|SD41
+72|SD42
+73|SD43
+74|SD44
+75|SD45
+76|SD46
+77|SD47
+78|SD48
+79|SD49
+80|SD50',
+  'member_register' => '1',
+  'member_status' => '1',
+  'member_regcode' => '0',
+  'member_logincode' => '0',
+  'diy_url' => '0',
+  'smtpserver' => 'smtp.163.com',
+  'smtpserverport' => '25',
+  'smtpusermail' => '',
+  'smtpuser' => 'admin',
+  'smtppass' => 'admin',
+  'smtpemailto' => '',
+  'rand_code' => 'f5a9e420ecb5af0bfdb74ea168a5eadc',
+);
---
+++ data/config/database.ini.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('IN_XIAOCMS')) exit();
+return array (
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => 'root',
+  'dbname' => 'edu',
+  'prefix' => 'huijia_',
+  'port' => '3306',
+  'charset' => 'utf8',
+);
